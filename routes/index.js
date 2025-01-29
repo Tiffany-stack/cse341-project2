bfbb8382db7315b959223a8f35d22f6ee63d3efb
Tiffany-stack <noc23001@byui.edu>
2025-01-29 23:48:27 +0800

    week 3 part 1

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,20 @@
-const router =  require('express').Router();
+const router = require('express').Router();
 
 router.use('/', require('./swagger'));
 
-router.get('/',(req, res) => {
-    //#swagger.tags=['Hello World']
-    res.send('Hello World');
+// Basic Hello World endpoint
+router.get('/', (req, res) => {
+    //#swagger.tags = ['Hello World']
+    res.send("Hello World!");
 });
 
-router.use('/users', require('./users'));
+// Register the routes for members and coaches
+router.use('/members', require('./members'));
+router.use('/coaches', require('./coaches'));
+
+// Catch-all handler for undefined routes (good practice to handle unknown routes)
+router.use((req, res) => {
+    res.status(404).json({ message: "Route not found" });
+});
 
 module.exports = router;
